0d2ccefc0ea85a10997d87fd0103a3f9aaa36e57
OrPh <orlovf143@gmail.com>
2025-06-28 13:15:36 +0300

    Add task05

---
+++ task05tests/task05tests.cs
@@ -0,0 +1,77 @@
+﻿namespace task05tests;
+
+using Xunit;
+using task05;
+
+public class TestClass
+{
+    public int PublicField;
+    private string _privateField = string.Empty;
+    public int Property { get; set; }
+
+    public void Method() { }
+    public void MethodWithParams(int x, string y) { }
+}
+
+[Serializable]
+public class AttributedClass { }
+
+public class ClassAnalyzerTests
+{
+    [Fact]
+    public void GetPublicMethods_ReturnsCorrectMethods()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        var methods = analyzer.GetPublicMethods();
+
+        Assert.Contains("Method", methods);
+        Assert.Contains("MethodWithParams", methods);
+    }
+
+    [Fact]
+    public void GetAllFields_IncludesPrivateFields()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        var fields = analyzer.GetAllFields();
+
+        Assert.Contains("_privateField", fields);
+    }
+
+    [Fact]
+    public void GetProperties_ReturnsCorrectProperties()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        var properties = analyzer.GetProperties();
+
+        Assert.Contains("Property", properties);
+    }
+
+    [Fact]
+    public void HasAttribute_ReturnsTrueForAttributedClass()
+    {
+        var analyzer = new ClassAnalyzer(typeof(AttributedClass));
+        bool hasAttribute = analyzer.HasAttribute<SerializableAttribute>();
+
+        Assert.True(hasAttribute);
+    }
+
+    [Fact]
+    public void HasAttribute_ReturnsFalseForNonAttributedClass()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        bool hasAttribute = analyzer.HasAttribute<SerializableAttribute>();
+
+        Assert.False(hasAttribute);
+    }
+
+    [Fact]
+    public void GetMethodParams_ReturnsCorrectParameters()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        var paramsList = analyzer.GetMethodParams("MethodWithParams").ToList();
+
+        Assert.Contains("x", paramsList);
+        Assert.Contains("y", paramsList);
+        Assert.Equal(2, paramsList.Count); 
+    }
+}
